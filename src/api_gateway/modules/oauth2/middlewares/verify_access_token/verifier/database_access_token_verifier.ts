ec1175b75e9f279246a7c0b58533d771c0b2f4ba
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-04-14 16:30:21 +0700

    implement check token validity

--- src/api_gateway/modules/oauth2/middlewares/verify_access_token/verifier/database_access_token_verifier.ts
+++ src/api_gateway/modules/oauth2/middlewares/verify_access_token/verifier/database_access_token_verifier.ts
@@ -1,7 +1,19 @@
+import { UnauthorizedError } from '@iredium/butterfly/lib/errors'
+import { CredentialService } from '~/src/api_gateway/modules/oauth2/services/credential'
 import { BaseVerifier } from '~/src/api_gateway/modules/oauth2/middlewares/verify_access_token/verifier/base_verifier'
 
 export class DatabaseAccessTokenVerifier extends BaseVerifier {
   protected async checkTokenValidity (): Promise<void> {
-
+    const credentials = new CredentialService()
+    const token = await credentials.find({
+      query: {
+        token: this.token,
+        active: true,
+        expires_at: {
+          $gte: new Date()
+        }
+      }
+    })
+    if (!token.length) throw new UnauthorizedError(this.invalidTokenErrorMessage)
   }
 }
