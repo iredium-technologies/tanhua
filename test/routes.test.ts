3c71f6fad39ddab2c0e815e6c109a1e860258c30
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-04-14 14:47:44 +0700

    use non async

--- test/routes.test.ts
+++ test/routes.test.ts
@@ -4,11 +4,11 @@ import request = require('supertest')
 const butterfly = initTest()
 
 describe('Routes Test', (): void => {
-  it('should respond 200 for GET /users', async (): Promise<void> => {
+  it('should respond 200 for GET /users', (): void => {
     request(butterfly.app).get('/users').expect(200)
   })
 
-  it('should respond 404 for GET /no-found-route', async (): Promise<void> => {
+  it('should respond 404 for GET /no-found-route', (): void => {
     request(butterfly.app).get('/no-found-route').expect(404)
   })
 })
