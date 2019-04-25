3a8f1efe37e396ac65326565830df06add2a657b
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-04-25 16:43:55 +0700

    add userId to request object

--- src/api_gateway/modules/oauth2/middlewares/verify_access_token/verifier/database_access_token_verifier.ts
+++ src/api_gateway/modules/oauth2/middlewares/verify_access_token/verifier/database_access_token_verifier.ts
@@ -1,11 +1,12 @@
+import { CredentialInterface } from './../../../models/credential/interface'
 import { UnauthorizedError } from '@iredium/butterfly/lib/errors'
 import { CredentialService } from '~/src/api_gateway/modules/oauth2/services/credential'
 import { BaseVerifier } from '~/src/api_gateway/modules/oauth2/middlewares/verify_access_token/verifier/base_verifier'
 
 export class DatabaseAccessTokenVerifier extends BaseVerifier {
-  protected async checkTokenValidity (): Promise<void> {
+  protected async checkTokenValidity (): Promise<CredentialInterface> {
     const credentials = new CredentialService()
-    const token = await credentials.find({
+    const credential = await credentials.find({
       query: {
         token: this.token,
         active: true,
@@ -14,6 +15,7 @@ export class DatabaseAccessTokenVerifier extends BaseVerifier {
         }
       }
     })
-    if (!token.length) throw new UnauthorizedError(this.invalidTokenErrorMessage)
+    if (!credential.length) throw new UnauthorizedError(this.invalidTokenErrorMessage)
+    return credential[0] as CredentialInterface
   }
 }
