c977637ffa7ad4d4978d37087e8064592cbba4c7
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-04-25 17:08:17 +0700

    remove vhost

--- src/api_gateway/modules/dashboard/routes.ts
+++ src/api_gateway/modules/dashboard/routes.ts
@@ -1,8 +1,7 @@
 import { RouteDrawer } from '@iredium/butterfly/lib/routes'
 import { handle } from '@iredium/butterfly/lib/routes/handle'
 import * as c from './controllers'
-import vhost = require('vhost')
 
 export function dashboardRoutes (routes: RouteDrawer): void {
-  routes.get('/oauth/applications', vhost(process.env.ACCOUNTS_HOST, handle(c.ApplicationsController, 'index')))
+  routes.get('/oauth/applications', handle(c.ApplicationsController, 'index'))
 }
