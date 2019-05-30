fd48a2efbb832456218038829fe4705e0daf81fa
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-05-31 02:25:44 +0700

    user created event handler

--- src/api_gateway/modules/accounts/listeners/users/created/index.ts
+++ src/api_gateway/modules/accounts/listeners/users/created/index.ts
@@ -1,8 +1,19 @@
+import { Kafka } from '@iredium/kafka-adapter'
 import { UserCreatedEvent } from '../../../events/users/created'
 import { BaseListener } from '@iredium/butterfly/lib/listeners'
 
 export class UserCreatedListener extends BaseListener {
-  public handle (event: UserCreatedEvent): void {
-    console.log({listened_event: event})
+  public async handle (event: UserCreatedEvent): Promise<void> {
+    this.kafkaSend(event)
+  }
+
+  protected kafkaSend (event): void {
+    const topic = 'users.user.created'
+    const kafka = new Kafka()
+    const payload = [{
+      messages: JSON.stringify(event),
+      topic
+    }]
+    kafka.send(payload)
   }
 }
