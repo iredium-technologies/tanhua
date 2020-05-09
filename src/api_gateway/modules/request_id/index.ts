46e6911d7520b5f45a1c5e5c2c1188956dbb03b4
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2020-05-09 17:09:28 +0700

    requestId from locals

--- src/api_gateway/modules/request_id/index.ts
+++ src/api_gateway/modules/request_id/index.ts
@@ -1,9 +1,9 @@
 export default function requestId ({ hook }): void {
   hook('tanhua:proxy:userResHeaderDecorator', function (headers, userReq, userRes, proxyReq, proxyRes): void {
-    headers['x-request-id'] = userReq['request_id']
+    headers['x-request-id'] = userRes.locals.requestId
   })
 
   hook('tanhua:proxy:proxyReqOptDecorator', function (proxyReqOpts, srcReq): void {
-    proxyReqOpts.headers['x-request-id'] = srcReq['request_id']
+    proxyReqOpts.headers['x-request-id'] = srcReq.headers['requestId']
   })
 }
