527a2bf6cde934130016b6fb15484c022adebb8d
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2020-05-17 10:13:38 +0700

    remove old user id proxy header

--- src/api_gateway/modules/user_id/index.ts
+++
@@ -1,7 +0,0 @@
-export default function userId ({ hook }): void {
-  hook('tanhua:proxy:proxyReqOptDecorator', function (proxyReqOpts, srcReq): void {
-    if (srcReq['authenticatedUserId']) {
-      proxyReqOpts.headers['x-authenticated-user-id'] = srcReq['authenticatedUserId']
-    }
-  })
-}
