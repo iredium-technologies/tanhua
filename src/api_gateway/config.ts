4bb30c21e911cabcc0bd8a3cb0d50bb17bddafd1
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2020-05-17 10:02:52 +0700

    add env config

--- src/api_gateway/config.ts
+++ src/api_gateway/config.ts
@@ -1,10 +1,32 @@
 import { UserService } from '~/src/api_gateway/modules/accounts/services/user'
 import { routes } from '~/src/api_gateway/config/routes'
 import { databases } from '~/src/api_gateway/config/databases'
+import dotenv = require('dotenv')
+import path = require('path')
+
+dotenv.config({
+  path: path.resolve(process.cwd(), process.env['NODE_ENV'] === 'test' ? '.env.test' : '.env')
+})
 
 export default {
   useViewEngine: true,
   userServiceClass: UserService,
   routes,
-  databases
+  databases,
+  env: {
+    NODE_ENV: process.env.NODE_ENV,
+    APP_NAME: process.env.APP_NAME,
+    PORT: process.env.PORT,
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    SESSION_COOKIE_DOMAIN: process.env.SESSION_COOKIE_DOMAIN,
+    MONGO_HOST: process.env.MONGO_HOST,
+    MONGO_PORT: process.env.MONGO_PORT,
+    MONGO_DATABASE: process.env.MONGO_DATABASE,
+    MONGO_USERNAME:process.env.MONGO_USERNAME,
+    MONGO_PASSWORD:process.env.MONGO_PASSWORD,
+    REDIS_HOST: process.env.REDIS_HOST,
+    REDIS_PORT: process.env.REDIS_PORT,
+    REDIS_PASSWORD:process.env.REDIS_PASSWORD,
+    JWT_SECRET:process.env.JWT_SECRET,
+  }
 }
