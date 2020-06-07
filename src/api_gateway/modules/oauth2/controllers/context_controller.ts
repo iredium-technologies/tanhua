d1cc80dc246954ce95a3bdc594e2c747e80cd182
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2020-06-08 05:04:55 +0700

    check for credential user id

--- src/api_gateway/modules/oauth2/controllers/context_controller.ts
+++ src/api_gateway/modules/oauth2/controllers/context_controller.ts
@@ -3,6 +3,8 @@ import { ContextPolicy } from '../policies/context'
 import { BaseResponse } from '@iredium/butterfly/lib/routes'
 import { JsonResponse } from '@iredium/butterfly/lib/routes/responses/json'
 import axios, { AxiosInstance } from 'axios'
+import { CredentialService } from '../services/credential'
+import { UnauthenticatedError, UnauthorizedError } from '@iredium/butterfly/lib/errors'
 
 export class ContextController extends ApiController {
   protected axios: AxiosInstance
@@ -16,6 +18,30 @@ export class ContextController extends ApiController {
 
   public async me (req): Promise<BaseResponse> {
     this.authorize('me')
+    const authorization = req.get('authorization')
+    const credentials = new CredentialService()
+    const splitted = authorization.split(' ')
+    let token
+
+    if (splitted.length === 2) { // TODO: check token format using regex
+      token = splitted[1]
+    } else {
+      throw new UnauthenticatedError()
+    }
+
+    const credential = await credentials.find({
+      query: {
+        token,
+        active: true,
+        expires_at: {
+          $gte: new Date()
+        }
+      }
+    })
+
+    if (!credential.length) throw new UnauthenticatedError()
+    if (!credential[0]['user_id'] || this.user._id.toString() !== credential[0]['user_id']) throw new UnauthorizedError()
+
     return new JsonResponse(req['locals']['user'])
   }
 }
